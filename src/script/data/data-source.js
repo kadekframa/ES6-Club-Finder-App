95dab29ac56134b428b0065c9277a22ae41887d3
kadekframa <dframa19@gmail.com>
2022-03-26 13:09:33 +0800

    ES6 JavaScript. Latihan cara penggunaan module pada JavaScript. Semangat !!!

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,3 +1,5 @@
+import clubs from "./clubs.js";
+
 class DataSource{
     static searchClub(keyword){
         return new Promise((resolve, reject) => {
@@ -12,3 +14,5 @@ class DataSource{
         
     };
 }
+
+export default DataSource;
